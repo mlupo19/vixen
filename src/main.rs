41405f6cdebe63e3d6b070d689451458cebabd5a
Mike <mike.lupo19@gmail.com>
2021-12-28 12:11:34 -0500

    Backface culling

--- src/main.rs
+++ src/main.rs
@@ -179,6 +179,7 @@ fn main() {
                 write: true,
                 .. Default::default()
             },
+            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
             .. Default::default()
         };
 
